826e52dc66d7a6b6a3b2ec43f4ec853da9fe153b
Benamin Peeters <holyvengeance.wow@gmail.com>
2015-01-27 19:45:58 +0100

    Location change
    Andere PC / locaties veranderen

--- Project_Prog4/src/project_prog4/Afrekenen.java
+++ Project_Prog4/src/project_prog4/Afrekenen.java
@@ -160,12 +160,12 @@ Zoeken zoek =  new Zoeken();
     }//GEN-LAST:event_lsbGebruikerValueChanged
 
     private void lsbBestellingValueChanged(javax.swing.event.ListSelectionEvent evt) {//GEN-FIRST:event_lsbBestellingValueChanged
-        if(lsbBestelling.getSelectedIndex() != -1)
-        {
-            Gerechten ger = zoek.ZoekGerechtByName(naam.get(lsbBestelling.getSelectedIndex()));
-            GerechtenList(ger);
-//            GebruikerList(ger);
-        }
+//        if(lsbBestelling.getSelectedIndex() != -1)
+//        {
+//            Gerechten ger = zoek.ZoekGerechtByName(naam.get(lsbBestelling.getSelectedIndex()));
+//            GerechtenList(ger);
+////            GebruikerList(ger);
+//        }
     }//GEN-LAST:event_lsbBestellingValueChanged
 
     private void GebruikerList()
